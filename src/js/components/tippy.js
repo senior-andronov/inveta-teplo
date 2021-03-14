3321d813e8a2de48cf55f04bdff9f9e8fc0bbfd8
awfulmark@yandex.ru <69839668+etojemaaark@users.noreply.github.com>
2021-03-14 14:57:22 +0500

    ф

--- src/js/components/tippy.js
+++ src/js/components/tippy.js
@@ -4,15 +4,20 @@ import tippy, { followCursor } from 'tippy.js';
 const template = document.querySelector('.feedback__tooltip');
 template.style.display = 'block';
 
-tippy('._tooltip', {
+tippy('._tooltip-feedback', {
 	content: template,
-	hideOnClick: true,
 	trigger: "click",
-	maxWidth: 250,
+	maxWidth: 400,
+	// placement: 'right-start',
+	interactive: true,
+	arrow: false,
 });
+
+
+
 tippy('._tooltip-services', {
 	content: "Зависит от необходимости подготовки основания, монтажа утеплителя и шага укладки труб",
-	hideOnClick: true,
 	trigger: "click",
 	maxWidth: 250,
+
 });
