90af6ccca6776173e58cb87e49c1a2cb04485249
etojemaaark <awfulmark@yandex.ru>
2021-03-15 13:10:24 +0500

    a

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -22,23 +22,119 @@ const feedback = new Swiper('.feedback__slider', {
 	},
 });
 
-const examplesTop = new Swiper('.examples__slider-top', {
+const examplesTopOne = new Swiper('.examples__slider-top-one', {
 	slidesPerView: 1,
 	spaceBetween: 30,
 	speed: 1000,
-	// thumbs: {
-	// 	swiper: examplesBot,
-	// }
+	loop: true,
+	loopedSlides: 5,
 });
-const examplesBot = new Swiper('.examples__slider-bot', {
-	slidesPerView: 1,
-	spaceBetween: 30,
+const examplesBotOne = new Swiper('.examples__slider-bot-one', {
+	slidesPerView: 3,
+	spaceBetween: 10,
 	speed: 1000,
-	// slidesPerView: 3,
-	// freeMode: true,
-	// watchSlidesVisibility: true,
-	// watchSlidesProgress: true,
+	loop: true,
+	loopedSlides: 5,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+	allowTouchMove: false,
+	// breakpoints: {
+	// 	0: {
+	// 		slidesPerView: 5,
+	// 	},
+	// 	1162: {
+	// 		slidesPerView: 3,
+	// 	},
+	// },
 });
 
-// examplesTop.controller.control = examplesBot;
-// examplesBot.controller.control = examplesTop;
+examplesTopOne.controller.control = examplesBotOne;
+examplesBotOne.controller.control = examplesTopOne;
+
+const examplesTopTwo = new Swiper('.examples__slider-top-two', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	speed: 1000,
+	loop: true,
+	loopedSlides: 5,
+});
+const examplesBotTwo = new Swiper('.examples__slider-bot-two', {
+	slidesPerView: 3,
+	spaceBetween: 10,
+	speed: 1000,
+	loop: true,
+
+	loopedSlides: 5,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+	allowTouchMove: false,
+});
+
+examplesTopTwo.controller.control = examplesBotTwo;
+examplesBotTwo.controller.control = examplesTopTwo;
+
+const examplesTopThree = new Swiper('.examples__slider-top-three', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	speed: 1000,
+	loop: true,
+	loopedSlides: 5,
+});
+const examplesBotThree = new Swiper('.examples__slider-bot-three', {
+	slidesPerView: 3,
+	spaceBetween: 10,
+	speed: 1000,
+	loop: true,
+
+	loopedSlides: 5,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+	allowTouchMove: false,
+});
+
+examplesTopThree.controller.control = examplesBotThree;
+examplesBotThree.controller.control = examplesTopThree;
+
+const examplesTopFour = new Swiper('.examples__slider-top-four', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	speed: 1000,
+	loop: true,
+	loopedSlides: 5,
+});
+const examplesBotFour = new Swiper('.examples__slider-bot-four', {
+	slidesPerView: 3,
+	spaceBetween: 10,
+	speed: 1000,
+	loop: true,
+
+	loopedSlides: 5,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+	allowTouchMove: false,
+});
+
+examplesTopFour.controller.control = examplesBotFour;
+examplesBotFour.controller.control = examplesTopFour;
+
+const examplesTopFive = new Swiper('.examples__slider-top-five', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	speed: 1000,
+	loop: true,
+	loopedSlides: 5,
+});
+const examplesBotFive = new Swiper('.examples__slider-bot-five', {
+	slidesPerView: 3,
+	spaceBetween: 10,
+	speed: 1000,
+	loop: true,
+
+	loopedSlides: 5,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+	allowTouchMove: false,
+});
+
+examplesTopFive.controller.control = examplesBotFive;
+examplesBotFive.controller.control = examplesTopFive;
