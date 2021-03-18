93461e85e289a58b77d032b7760fd74000312bf7
etojemaaark <awfulmark@yandex.ru>
2021-03-18 14:44:46 +0500

    a

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,6 +1,12 @@
 let menuBurger = document.querySelector('.header__burger');
-let menu = document.querySelector('.header-mob-menu');
+let menu = document.querySelector('.header__mob-menu');
+let headerMob = document.querySelector('.header');
+let headerLogoDesc = document.querySelector('.header__logo-desc');
+let headerLogoMob = document.querySelector('.header__logo-mob');
 menuBurger.addEventListener('click', function () {
 	menuBurger.classList.toggle('_active');
 	menu.classList.toggle('_active');
+	headerMob.classList.toggle('_active');
+	headerLogoDesc.classList.toggle('_active');
+	headerLogoMob.classList.toggle('_active');
 })
--- src/js/components/modals.js
+++ src/js/components/modals.js
@@ -1,94 +1,131 @@
-let callbackModal = document.querySelector(".callback__modal");
-let callbackBtn = document.querySelector(".callback__btn");
-let callbackCross = document.querySelector(".callback__modal-close");
-let callbackContent = document.querySelector(".callback__modal-content");
+// let callbackModal = document.querySelector(".callback__modal");
+// let callbackBtn = document.querySelector(".callback__btn");
+// let callbackCross = document.querySelector(".callback__modal-close");
+// let callbackContent = document.querySelector(".callback__modal-content");
 
-callbackBtn.onclick = function () {
-	callbackModal.classList.add('_active');
-	animateCSS(callbackContent, 'animateTop');
-	document.body.classList.add("active");
+// callbackBtn.onclick = function () {
+// 	callbackModal.classList.add('_active');
+// 	animateCSS(callbackContent, 'animateTop');
+// 	document.body.classList.add("active");
+// }
+// callbackCross.onclick = function () {
+// 	callbackModal.classList.remove('_active');
+// 	document.body.classList.remove("active");
+// }
+
+// let pricelistModal = document.querySelector(".pricelist__modal");
+// let pricelistBtn = document.querySelectorAll(".pricelist__btn");
+// let pricelistCross = document.querySelector(".pricelist__modal-close");
+
+// pricelistBtn.forEach(btn => {
+// 	btn.onclick = function () {
+// 		pricelistModal.classList.add('_active');
+// 		document.body.classList.add("active");
+// 	}
+// })
+
+// pricelistCross.onclick = function () {
+// 	pricelistModal.classList.remove('_active');
+// 	document.body.classList.remove("active");
+// }
+
+// let privacyModal = document.querySelector(".privacy__modal");
+// let privacyBtn = document.querySelectorAll(".privacy__btn");
+// let privacyCross = document.querySelector(".privacy__modal-close");
+
+// privacyBtn.forEach(btn => {
+// 	btn.onclick = function () {
+// 		privacyModal.classList.add('_active');
+// 		document.body.classList.add("active");
+// 	}
+// })
+// privacyCross.onclick = function () {
+// 	if (callbackModal.classList.contains('_active') || feedbackModal.classList.contains('_active') || pricelistModal.classList.contains('_active')) {
+// 		privacyModal.classList.remove('_active');
+// 	} else {
+// 		privacyModal.classList.remove('_active');
+// 		document.body.classList.remove("active");
+// 	}
+// }
+
+// let feedbackModal = document.querySelector(".feedback__modal");
+// let feedbackBtn = document.querySelectorAll(".feedback__btn");
+// let feedbackCross = document.querySelector(".feedback__modal-close");
+// feedbackBtn.forEach(btn => {
+// 	btn.onclick = function () {
+// 		feedbackModal.classList.add('_active');
+// 		document.body.classList.add("active");
+// 	}
+// })
+
+// feedbackCross.onclick = function () {
+// 	feedbackModal.classList.remove('_active');
+// 	document.body.classList.remove("active");
+// }
+
+// window.onclick = function (event) {
+// 	if (event.target == pricelistModal) {
+// 		pricelistModal.classList.remove('_active');
+// 		document.body.classList.remove("active");
+
+// 	}
+// 	if (event.target == callbackModal) {
+// 		callbackModal.classList.remove('_active');
+// 		document.body.classList.remove("active");
+// 	}
+// 	if (event.target == feedbackModal) {
+// 		feedbackModal.classList.remove('_active');
+// 		document.body.classList.remove("active");
+// 	}
+// 	if (event.target == privacyModal) {
+// 		if (callbackModal.classList.contains('_active') || feedbackModal.classList.contains('_active') || pricelistModal.classList.contains('_active')) {
+// 			privacyModal.classList.remove('_active');
+// 		} else {
+// 			privacyModal.classList.remove('_active');
+// 			document.body.classList.remove("active");
+// 		}
+// 	}
+// }
+
+
+function modal() {
+	const btns = document.querySelectorAll('.modal-button-js'),
+		close = document.querySelectorAll('.modal__close');
+
+	function checkModal() {
+		const modal = document.querySelector(`#${this.dataset.modal}`);
+		showModal(modal);
+	}
+
+	function showModal(parent) {
+		const content = parent.querySelector('.modal__content');
+		parent.classList.add('_active')
+		animateCSS(content, 'animateTop');
+		addClassBody();
+	}
+
+	function closeModal() {
+		const modalActive = document.querySelector('.modal._active')
+		animateCSS(modalActive, 'animateUp');
+		modalActive.addEventListener('animationend', () => {
+			modalActive.classList.remove('_active');
+		}, { once: true })
+		addClassBody();
+	}
+
+	function addClassBody() {
+		(document.body.classList.contains('active')) ? document.body.classList.remove('active') : document.body.classList.add('active')
+	}
+
+	function searchModal(event) {
+		if (event.target == document.querySelector('.modal._active')) closeModal()
+	}
+
+	btns.forEach(item => item.addEventListener('click', checkModal));
+	close.forEach(item => item.addEventListener('click', closeModal));
+	window.addEventListener('click', searchModal)
 }
-callbackCross.onclick = function () {
-	callbackModal.classList.remove('_active');
-	document.body.classList.remove("active");
-}
-
-let pricelistModal = document.querySelector(".pricelist__modal");
-let pricelistBtn = document.querySelectorAll(".pricelist__btn");
-let pricelistCross = document.querySelector(".pricelist__modal-close");
-
-pricelistBtn.forEach(btn => {
-	btn.onclick = function () {
-		pricelistModal.classList.add('_active');
-		document.body.classList.add("active");
-	}
-})
-
-pricelistCross.onclick = function () {
-	pricelistModal.classList.remove('_active');
-	document.body.classList.remove("active");
-}
-
-let privacyModal = document.querySelector(".privacy__modal");
-let privacyBtn = document.querySelectorAll(".privacy__btn");
-let privacyCross = document.querySelector(".privacy__modal-close");
-
-privacyBtn.forEach(btn => {
-	btn.onclick = function () {
-		privacyModal.classList.add('_active');
-		document.body.classList.add("active");
-	}
-})
-privacyCross.onclick = function () {
-	if (callbackModal.classList.contains('_active') || feedbackModal.classList.contains('_active') || pricelistModal.classList.contains('_active')) {
-		privacyModal.classList.remove('_active');
-	} else {
-		privacyModal.classList.remove('_active');
-		document.body.classList.remove("active");
-	}
-}
-
-let feedbackModal = document.querySelector(".feedback__modal");
-let feedbackBtn = document.querySelectorAll(".feedback__btn");
-let feedbackCross = document.querySelector(".feedback__modal-close");
-feedbackBtn.forEach(btn => {
-	btn.onclick = function () {
-		feedbackModal.classList.add('_active');
-		document.body.classList.add("active");
-	}
-})
-
-feedbackCross.onclick = function () {
-	feedbackModal.classList.remove('_active');
-	document.body.classList.remove("active");
-}
-
-window.onclick = function (event) {
-	if (event.target == pricelistModal) {
-		pricelistModal.classList.remove('_active');
-		document.body.classList.remove("active");
-
-	}
-	if (event.target == callbackModal) {
-		callbackModal.classList.remove('_active');
-		document.body.classList.remove("active");
-	}
-	if (event.target == feedbackModal) {
-		feedbackModal.classList.remove('_active');
-		document.body.classList.remove("active");
-	}
-	if (event.target == privacyModal) {
-		if (callbackModal.classList.contains('_active') || feedbackModal.classList.contains('_active') || pricelistModal.classList.contains('_active')) {
-			privacyModal.classList.remove('_active');
-		} else {
-			privacyModal.classList.remove('_active');
-			document.body.classList.remove("active");
-		}
-	}
-}
-
-
-// animateCSS(document.querySelector(".callback__modal.feedback__modal-content"), '.a-fade-up');
+modal();
 
 
 function animateCSS(element, animation) {
