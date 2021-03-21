d59ae4d5bf6bc7693d34a07e219e3a4563f3582e
awfulmark@yandex.ru <69839668+etojemaaark@users.noreply.github.com>
2021-03-21 21:43:29 +0500

    a

--- src/js/components/anchors.js
+++ src/js/components/anchors.js
@@ -2,6 +2,7 @@ function scrollAnchors() {
 	const anchors = document.querySelectorAll('a[href*="#"]')
 	let menuBurger = document.querySelector('.header__burger');
 	let menu = document.querySelector('.header__mob-menu');
+	let header = document.querySelector('.header');
 	anchors.forEach(anchor => {
 		anchor.addEventListener('click', function (e) {
 			e.preventDefault()
@@ -11,6 +12,7 @@ function scrollAnchors() {
 				menuBurger.classList.remove('_active')
 				menu.classList.remove('_active')
 				document.body.classList.remove('_lock');
+				// header.classList.remove('_active');
 			}
 			document.querySelector(block).scrollIntoView({
 				behavior: 'smooth',
