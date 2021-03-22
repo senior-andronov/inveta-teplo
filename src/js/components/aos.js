1a063739d12dfc8a10343c93ddfff22cfdf6c88c
etojemaaark <awfulmark@yandex.ru>
2021-03-22 20:09:01 +0500

    ф

--- src/js/components/aos.js
+++ src/js/components/aos.js
@@ -11,7 +11,7 @@ AOS.init({
 	debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
 	throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-	offset: 20, // offset (in px) from the original trigger point
+	offset: 100, // offset (in px) from the original trigger point
 	delay: 0, // values from 0 to 3000, with step 50ms
 	duration: 400, // values from 0 to 3000, with step 50ms
 	easing: 'ease', // default easing for AOS animations
