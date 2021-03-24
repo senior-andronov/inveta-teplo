bd292c7524dcd7575639d1d9c89eb86d65ad20d9
etojemaaark <awfulmark@yandex.ru>
2021-03-24 11:43:39 +0500

    a

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -5,7 +5,7 @@ function scrollAnchors() {
 	anchors.forEach(anchor => {
 		anchor.addEventListener('click', function (e) {
 			e.preventDefault()
-			burger()
+			closeBurger()
 			const block = anchor.getAttribute('href');
 			if (block == '#') return;
 			if (menuBurger.classList.contains('_active')) {
@@ -29,28 +29,25 @@ let headerLogoDesc = document.querySelector('.header__logo-desc');
 let headerLogoMob = document.querySelector('.header__logo-mob');
 
 function burger() {
-	menuBurger.classList.toggle('_active');
-	menu.classList.toggle('_active');
-	if (document.documentElement.clientWidth < 1192) {
-		headerMob.classList.toggle('_active');
-		headerLogoDesc.classList.toggle('_active');
-		headerLogoMob.classList.toggle('_active');
-		document.body.classList.toggle('_lock');
+	if (menu.classList.contains('_active')) {
+		closeBurger()
+	} else {
+		headerMob.classList.add('_active');
+		headerLogoDesc.classList.add('_active');
+		headerLogoMob.classList.add('_active');
+		menu.classList.add('_active');
+		menuBurger.classList.add('_active');
+		document.body.classList.add('_lock');
 	}
 }
-// function removeMenu() {
-// 	menuBurger.classList.remove('_active');
-// 	headerMob.classList.remove('_active');
-// 	headerLogoDesc.classList.remove('_active');
-// 	headerLogoMob.classList.remove('_active');
-// 	document.body.classList.remove('_lock');
-// }
-// function removeMenu1(event) {
-// 	if (event.target == document.querySelector('.header__mob-menu._active')) removeMenu()
-// 	console.log(event.target != document.querySelector('.header__mob-menu._active'));
-
-// }
-// window.addEventListener('click', removeMenu1)
+function closeBurger() {
+	headerMob.classList.remove('_active');
+	headerLogoDesc.classList.remove('_active');
+	headerLogoMob.classList.remove('_active');
+	menu.classList.remove('_active');
+	menuBurger.classList.remove('_active');
+	document.body.classList.remove('_lock');
+}
 window.addEventListener('click', function (e) {
 	if (!headerMob.contains(e.target) && !menuBurger.contains(e.target) && headerMob.classList.contains('_active')) {
 		burger();
