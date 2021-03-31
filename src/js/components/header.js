924e2a2b46c9c290a6075dde6206381abfe7ea9f
etojemaaark <awfulmark@yandex.ru>
2021-03-31 15:09:26 +0500

    a

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -16,7 +16,7 @@ function headerFixed() {
 		document.addEventListener('scroll', function () {
 			checkHeight();
 		});
-		checkHeight();
+		// checkHeight();
 		function checkHeight() {
 			if (lostY >= 110) {
 				if (window.scrollY > lostY) {
@@ -42,3 +42,53 @@ function headerFixed() {
 	});
 }
 headerFixed();
+
+
+// (function () {
+// 	let curScroll
+// 	let prevScroll = window.scrollY || document.documentElement.scrollTop
+// 	let curDirection = 0
+// 	let prevDirection = 0
+// 	let header = document.querySelector('.header')
+// 	let offsetHeight = header.offsetHeight
+// 	let toggled
+// 	let threshold = 110
+ 
+// 	let checkScroll = function () {
+// 		curScroll = window.scrollY || document.documentElement.scrollTop
+// 		if (curScroll > prevScroll) {
+// 			 // scrolled down
+// 			curDirection = 2
+// 		} else {
+// 			//scrolled up
+// 			curDirection = 1
+// 		}
+// 		if (curDirection !== prevDirection) {
+// 			toggled = toggleHeader()
+// 		}
+// 		prevScroll = curScroll
+// 		if (toggled) {
+// 			prevDirection = curDirection
+// 		}
+// 	};
+
+// 	let toggleHeader = function () {
+// 		toggled = true
+// 		if (curDirection === 2 && curScroll > threshold) {
+// 			if(window.innerWidth >= 1192){
+// 				header.classList.remove('_bg')
+// 			}
+// 			header.classList.add('_hide')
+// 			header.classList.remove('_bg')
+// 		}
+// 		else if (curDirection === 1) {
+// 			header.classList.remove('_hide')
+// 			header.classList.add('_bg')
+// 		}
+// 		else {
+// 			toggled = false
+// 		}
+// 		return toggled
+// 	};
+// 	window.addEventListener('scroll', checkScroll)
+// })();
